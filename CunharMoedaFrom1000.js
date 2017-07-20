63fc3f59f7d6086d333be653666fabf0017f9b77
Thiiaguinho <thiiaguinhomes@gmail.com>
2017-07-20 17:24:36 -0300

    Create CunharMoedaFrom1000.js

---
+++ CunharMoedaFrom1000.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Cunhar Moeda
+// @include      https://*&screen=snob&mode=coin*
+// ==/UserScript==
+
+(function () {
+/* Script Escrito por ThiioM :) */
+
+function aleatorio (inferior, superior) {
+    return Math.round(parseInt(inferior) + (Math.random() * (superior - inferior)));
+}
+  
+function cunhar() {
+        setTimeout(function () { $('#select_anchor_top').click(); }, 1000);
+        setTimeout(function () { $('.mint_multi_button').eq(0).click(); }, 2000);
+}
+
+let tempo = aleatorio(70000, 120000);
+
+setInterval(
+  function() {
+   $('a.paged-nav-item').eq(0)[0].click();
+  }, tempo);
+})();
